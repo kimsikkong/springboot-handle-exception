9d3af7953f29919ae0ab49f94b37c5aca27e8002
kimsikkong <kimsikkong@gmail.com>
2021-08-18 23:10:44 +0900

    initial commit

---
+++ src/main/java/com/example/springboothandleexception/SpringbootHandleExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothandleexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHandleExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHandleExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboothandleexception/SpringbootHandleExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothandleexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHandleExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
